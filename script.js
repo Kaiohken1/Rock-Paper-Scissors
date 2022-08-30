76c0c1afd1617d96b38a467000c4f7d1510e3c9d
Ryan <raygsm@live.fr>
2022-08-30 21:43:27 +0200

    Fix upper case on certain strings

--- script.js
+++ script.js
@@ -10,24 +10,24 @@ function playRound(playerSelection, computerSelection) {
     if (playerSelection === computerSelection) {
         return `It's a tie ! You both picked ${playerSelection}`;
     } else if (playerSelection === "rock" && computerSelection === "scissors") {
-        return "You win ! rock beat scissors"
+        return "You win ! Rock beat Scissors"
     } else if (playerSelection === "rock" && computerSelection === "paper") {
-        return "You Lose ! paper beats rock"
+        return "You Lose ! Paper beats Rock"
     } else if (playerSelection === "scissors" && computerSelection === "paper") {
-        return "You win ! scissors beats paper"
+        return "You win ! Scissors beats Paper"
     } else if (playerSelection === "scissors" && computerSelection === "rock") {
-        return "You Lose ! rock beats scissors"
+        return "You Lose ! Rock beats Scissors"
     } else if (playerSelection === "paper" && computerSelection === "rock") {
-        return "You win ! paper beat rock"
+        return "You win ! Paper beat Rock"
     } else if (playerSelection === "paper" && computerSelection === "scissors") {
-        return "You Lose ! scissors beats paper"
+        return "You Lose ! Scissors beats Paper"
     }
 }
 
 //Loop the game for five rounds maximum
 function game() {
     for (let i = 0; i <5; i++) {
-        let playerSelection = prompt("Choose between: rock, paper or scissors").toLocaleLowerCase();
+        let playerSelection = prompt("Choose between: Rock, Paper or Scissors").toLocaleLowerCase();
         let computerSelection = getComputerChoice()
         console.log(playRound(playerSelection, computerSelection));
     }
